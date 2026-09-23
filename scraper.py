a3b272ab59177ed26db3edc2d03c243d8312637a
dherrera7896-ui <dherrera7896@gmail.com>
2026-09-23 00:46:35 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
 from datetime import datetime
+from bs4 import BeautifulSoup
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import BeautifulSoup, By
+from selenium.webdriver.common.by import By
 
 print('--- INICIANDO EXTRACCIÓN REAL CON SELENIUM (SEACE) ---')
 
